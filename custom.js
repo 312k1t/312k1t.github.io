2600c3af04e343142b66e69c0d2043ab5c33418e
CZ\Jraska <Jraska@moravia.com>
2019-04-29 10:42:42 +0200

    add html

--- custom.js
+++ custom.js
@@ -1,1 +1,9 @@
-alert("custom script executed!");
+(function() {
+	var customFieldElement = document.getElementById('custom-fields-content');
+	customFieldElement.innerHTML = '';
+	var iframe = document.createElement('iframe');
+	iframe.frameBorder = 0;
+	iframe.src = 'https://www.google.com/';	
+	customFieldElement.appendChild(iframe);
+})();
+
