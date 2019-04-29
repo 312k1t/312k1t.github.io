f70d36afe4e88956f6aa67ca44e34c8d405941f8
CZ\Jraska <Jraska@moravia.com>
2019-04-29 10:51:26 +0200

    styling

--- custom.js
+++ custom.js
@@ -4,6 +4,8 @@
 	var iframe = document.createElement('iframe');
 	iframe.frameBorder = 0;
 	iframe.src = 'https://312k1t.github.io/index.html';	
+	iframe.scrolling = "no";
+	iframe.style = "overflow:hidden;width:100%;height:500px";
 	customFieldElement.appendChild(iframe);
 })();
 
