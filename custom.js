efe0e08dbb065519beeb181430595dc132d0b32c
CZ\Jraska <Jraska@moravia.com>
2019-05-09 17:03:59 +0200

    prototype 2nd option

--- custom.js
+++ custom.js
@@ -1,11 +1,16 @@
-(function() {
-	var customFieldElement = document.getElementById('custom-fields-content');
-	customFieldElement.innerHTML = '';
-	var iframe = document.createElement('iframe');
-	iframe.frameBorder = 0;
-	iframe.src = 'https://312k1t.github.io/index.html';	
-	iframe.scrolling = "no";
-	iframe.style = "overflow:hidden;width:100%;height:500px";
-	customFieldElement.appendChild(iframe);
-})();
+// (function() {
+	// var customFieldElement = document.getElementById('custom-fields-content');
+	// customFieldElement.innerHTML = '';
+	// var iframe = document.createElement('iframe');
+	// iframe.frameBorder = 0;
+	// iframe.src = 'https://312k1t.github.io/index.html';	
+	// iframe.scrolling = "no";
+	// iframe.style = "overflow:hidden;width:100%;height:500px";
+	// customFieldElement.appendChild(iframe);
+// })();
 
+
+$(document).ready(function() {
+	$('#custom-fields-content input').attr("readonly", "readonly");
+	$('#customContent').prepend('<a href="https://312k1t.github.io/index.html" type="button" class="btn btn-default" target="_blank">Edit custom fields</a>');
+});
